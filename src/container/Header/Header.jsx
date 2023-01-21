a5528cfb7a0d360645eec7d0a214c7488176df58
iambasilp <basilxg321@gmail.com>
2023-01-21 18:10:53 +0530

    start with section Header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-
+import SubHeading from '../../components/SubHeading/SubHeading';
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding" id='home'>
+    <div className="app__content app__wrapper_info">
+      <SubHeading title="Chase the new Flavour" />
+      <h1 className='app__header-h1'>The key to Fine dining</h1>
+    </div>
+    <div className="app__image"></div>
   </div>
 );
 
