64791890b041157b1b9e074e0795437bee88bc6b
iambasilp <basilxg321@gmail.com>
2023-01-22 12:55:38 +0530

    Header section added

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import SubHeading from '../../components/SubHeading/SubHeading';
+import images from '../../constants/images';
 import './Header.css';
 
 const Header = () => (
@@ -7,8 +8,12 @@ const Header = () => (
     <div className="app__content app__wrapper_info">
       <SubHeading title="Chase the new Flavour" />
       <h1 className='app__header-h1'>The key to Fine dining</h1>
+      <p className="p__opensans" style={{margin:'2rem 0'}}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
+      <button type='button' className='custom__button'>Explore Menu</button>
     </div>
-    <div className="app__image"></div>
+    <div className="app__wrapper_img app__header-image">
+        <img src={images.welcome} alt="" />
+    </div>
   </div>
 );
 
