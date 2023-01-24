08f5604e953419092389a6a406595c0e8dfd21c3
iambasilp <basilxg321@gmail.com>
2023-01-24 10:52:52 +0530

    Gallary section completed

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,45 +1,45 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import {BsInstagram, BsArrowLeftShort, BsArrowRightShort} from 'react-icons/bs'
 import {images} from '../../constants';
 import {SubHeading} from '../../components'
 import './Gallery.css';
 
-const GallaryImages = [images.gallery01, images.gallery02, images.gallery03, images.gallery04]
-
-const Gallery = () => {
-  const scrollRef = React.useRef()
+const GallaryImage = [images.gallery01, images.gallery02, images.gallery03, images.gallery04]
+const Gallary = ()=>{
+  const refImage = useRef()
   const scroll = (direction)=>{
-     const {current} = scrollRef;
-     if(direction === 'left'){
-      current.scrollLeft -= 300;
-     }else{
-      current.scrollLeft += 300;
-     }
+    const {current} = refImage
+    if(direction=== 'left'){
+      current.scrollLeft -=300
+    }else{
+      current.scrollLeft +=300
+    }
   }
   return (
-    <div className="app__gallary flex__center">
-      <div className='app__gallary-content'>
+    <div className="app__gallary flex__center app__wrapper">
+      <div className="app__gallary-content">
         <SubHeading title="Instagram" />
         <h1 className='headtext__cormorant'>Photo Gallery</h1>
-        <p className='p__opensans' style={{color:"#AAA"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mu.</p>
+        <p className='p__opensans'>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum laboriosam voluptate totam. Deserunt mollitia laboriosam, enim quia dolores praesentium at?
+        </p>
         <button type='button' className='custom__button'>View More</button>
       </div>
-      <div className="app__gallary-images">
-        <div className="app__gallary-images_container" ref={scrollRef}>
-            {GallaryImages.map((image,index)=>(
-                <div className="app__gallary-images_card flex__center" key={`gallary_images-${index + 1}`}>
-                  <img src={image} alt="gallary images" />
-                  <BsInstagram className='gallary__img-icon' />
-                </div>
-            ))}
+      <div className="app__gallary-image">
+        <div className="app__gallary-image-container" ref={refImage}>
+          {GallaryImage.map((image)=> (
+            <div className="app__gallary-image_card flex__center">
+              <img src={image} alt="gallary image" />
+              <BsInstagram color='#fff' className='app__gallary-icon'/>
+            </div>
+          ))}
         </div>
-        <div className='app__gallary-images_arrows'>
-          <BsArrowLeftShort className='gallary__arrow-icons' onClick={()=> scroll('left')} />
-          <BsArrowRightShort className='gallary__arrow-icons' onClick={()=> scroll('right')} />
+        <div className="app__gallary-image-overlay">
+        <BsArrowLeftShort className='overlay-icon' onClick={()=> scroll('left')} />
+        <BsArrowRightShort className='overlay-icon' onClick={()=> scroll('right')}/>
         </div>
       </div>
     </div>
   )
 }
-
-export default Gallery;
+export default Gallary
